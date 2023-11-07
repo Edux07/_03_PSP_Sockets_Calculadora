5471f075fdfe7fd66bc2f2b3ee21c0f53c9ec3c1
Edu <118288657+Edux07@users.noreply.github.com>
2023-11-07 17:32:52 +0100

    .....

--- src/Cliente/SocketCliente.java
+++ src/Cliente/SocketCliente.java
@@ -1,5 +1,46 @@
 package Cliente;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class SocketCliente {
 
+	public static final int PUERTO = 2068;
+	public static final String IP_SERVER = "Localhost";
+
+	public static void main(String[] args) {
+		System.out.println("        CALCULADORA         ");
+		System.out.println("----------------------------");
+
+		InetSocketAddress direccionServercalcu = new InetSocketAddress(IP_SERVER, PUERTO);
+
+		try (Scanner sc = new Scanner(System.in); Socket socketAlServidor = new Socket()) {
+
+				
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		} catch (UnknownHostException e) {
+			System.err.println("CLIENTE: No encuentro el servidor en la dirección" + IP_SERVER);
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("CLIENTE: Error de entrada/salida");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("CLIENTE: Error -> " + e);
+			e.printStackTrace();
+		}
+	}
 }
