65d2d8bf0a59d593fd8ba6099a63cf72398f2e24
Edu <118288657+Edux07@users.noreply.github.com>
2023-11-09 20:13:26 +0100

    trimer try

--- src/Cliente/SocketCliente.java
+++ src/Cliente/SocketCliente.java
@@ -20,26 +20,47 @@ public class SocketCliente {
 
 		InetSocketAddress direccionServercalcu = new InetSocketAddress(IP_SERVER, PUERTO);
 
+		try (Socket SocketAlServer = new Socket(); Scanner sc = new Scanner(System.in)) {
 
-        try (Socket SocketAlServer = new Socket();
-             BufferedReader entrada = new BufferedReader(new InputStreamReader(SocketAlServer.getInputStream()));
-             BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in)) ) {
+			while (true) {
+				System.out.println("Operaciones disponibles:");
+				System.out.println("1. Sumar");
+				System.out.println("2. Restar");
+				System.out.println("3. Multiplicar");
+				System.out.println("4. Dividir");
+				System.out.println("5. Salir");
+				System.out.print("Seleccione una opción: ");
 
-            while (true) {
-                System.out.println("Operaciones disponibles:");
-                System.out.println("1. Sumar");
-                System.out.println("2. Restar");
-                System.out.println("3. Multiplicar");
-                System.out.println("4. Dividir");
-                System.out.println("5. Salir");
-                System.out.print("Seleccione una opción: ");
-                String opcion = teclado.readLine();
-                
-                
-                
-                
-            }
-            
+				String opcion = sc.nextLine();
+
+				if (opcion.equals("5")) {
+					System.out.println("Saliendo del cliente.");
+					break;
+
+				} else if (opcion.equals("1") || opcion.equals("2") || opcion.equals("3") || opcion.equals("4")) {
+					System.out.print("Ingrese el primer número: ");
+					double numero1 = Double.parseDouble(sc.nextLine());
+					System.out.print("Ingrese el segundo número: ");
+					double numero2 = Double.parseDouble(sc.nextLine());
+
+					System.out.println("CLIENTE: Esperando a que el servidor acepte la conexión");
+					SocketAlServer.connect(direccionServercalcu);
+					System.out.println("CLIENTE: Conexion establecida... a " + IP_SERVER + " por el puerto " + PUERTO);
+
+					PrintStream salida = new PrintStream(SocketAlServer.getOutputStream());
+					salida.println(opcion);
+					salida.println(Double.toString(numero1));
+					salida.println(Double.toString(numero2));
+
+				}
+				InputStreamReader entrada = new InputStreamReader(SocketAlServer.getInputStream());
+				BufferedReader bf = new BufferedReader(entrada);
+
+				System.out.println("CLIENTE: Esperando al resultado del servidor...");
+				String resultado = bf.readLine();
+
+				System.out.println("CLIENTE: El resultado de la suma es:" + resultado);
+			}
 
 		} catch (UnknownHostException e) {
 			System.err.println("CLIENTE: No encuentro el servidor en la dirección" + IP_SERVER);
