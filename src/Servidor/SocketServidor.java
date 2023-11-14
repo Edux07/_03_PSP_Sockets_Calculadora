e866b5dea44b0c80b3cab736b1550c85691076ba
Edu <118288657+Edux07@users.noreply.github.com>
2023-11-14 15:47:53 +0100

    ..

--- src/Servidor/SocketServidor.java
+++ src/Servidor/SocketServidor.java
@@ -8,11 +8,10 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 public class SocketServidor {
 	public static final int PUERTO = 2068;
 
-	public static void main(String[] args) throws InterruptedException{
+	public static void main(String[] args) throws InterruptedException {
 
 		System.out.println("      SERVIDOR      ");
 		System.out.println("--------------------");
@@ -29,56 +28,50 @@ public class SocketServidor {
 			socketAlCliente = serverSocket.accept();
 
 			while (true) {
-				
-				 
+
 				if (socketAlCliente.isClosed()) {
 					socketAlCliente = serverSocket.accept();
 
-				}else {			
-					
-				System.out.println("SERVIDOR: Esperando peticion por el puerto " + PUERTO);
+				} else {
 
-				System.out.println("SERVIDOR: peticion numero " + ++peticion + " recibida");
+					System.out.println("SERVIDOR: Esperando peticion por el puerto " + PUERTO);
 
-				entrada = new InputStreamReader(socketAlCliente.getInputStream());
-				BufferedReader bf = new BufferedReader(entrada);
-				
+					System.out.println("SERVIDOR: peticion numero " + ++peticion + " recibida");
 
-				String opcion = bf.readLine();
-				System.out.println("SERVIDOR: Me ha llegado del cliente: "+ "La opcion" + opcion);
+					entrada = new InputStreamReader(socketAlCliente.getInputStream());
+					BufferedReader bf = new BufferedReader(entrada);
 
-				if(opcion.equals("5")) {
-					System.out.println("La aplicacion se cierra");
-					socketAlCliente.close();
-					
-				}else if (opcion.equals("1") || opcion.equals("2") || opcion.equals("3") || opcion.equals("4")) { 
-                    double numero1 = Double.parseDouble(bf.readLine());
-                    double numero2 = Double.parseDouble(bf.readLine());
+					String opcion = bf.readLine();
+					System.out.println("SERVIDOR: Me ha llegado del cliente: " + "La opcion" + opcion);
 
-                    double resultado = 0.0;
-                    
-					if(opcion.equals("1")) {
-						resultado= numero1 + numero2;
-					} else if(opcion.equals("2")) {
-						resultado= numero1 - numero2;
-					}else if(opcion.equals("3")) {
-						resultado= numero1 * numero2;
-					}else if(opcion.equals("4")) {
-						resultado = numero1 /numero2;
+					if (opcion.equals("5")) {
+						System.out.println("La aplicacion se cierra");
+						socketAlCliente.close();
+
+					} else if (opcion.equals("1") || opcion.equals("2") || opcion.equals("3") || opcion.equals("4")) {
+						double numero1 = Double.parseDouble(bf.readLine());
+						double numero2 = Double.parseDouble(bf.readLine());
+
+						double resultado = 0.0;
+
+						if (opcion.equals("1")) {
+							resultado = numero1 + numero2;
+						} else if (opcion.equals("2")) {
+							resultado = numero1 - numero2;
+						} else if (opcion.equals("3")) {
+							resultado = numero1 * numero2;
+						} else if (opcion.equals("4")) {
+							resultado = numero1 / numero2;
+						}
+						System.out.println("SERVIDOR: El calculo de los numeros es: " + resultado);
+						salida = new PrintStream(socketAlCliente.getOutputStream());
+						salida.println(resultado);
+
 					}
-					System.out.println("SERVIDOR: El calculo de los numeros es: " + resultado);	
-					salida = new PrintStream(socketAlCliente.getOutputStream());
-					salida.println(resultado);	
 
 				}
-				
-				
-				
-			}
 
-				
 			}
-
 
 		} catch (IOException e) {
 			System.err.println("SERVIDOR: Error de entrada/salida");
